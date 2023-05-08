f11524e6ba41df11e441d59939a189238f844a83
thelionthelion <widmerleo@student.ethz.ch>
2023-05-09 00:12:18 +0200

    add lion token

---
+++ LionToken.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.10;
+
+import "./interfaces/IERC20.sol";
+import "./libraries/SafeMath.sol";
+
+contract LionToken is IERC20 {
+  using SafeMath for uint256;
+
+  uint256 public constant _totalSupply = 10**13;
+  string public name = 'Lion Token';
+  uint8 public constant decimals = 10;
+  string public constant symbol = 'LION';
+  address public topAddress;                                            // Cache address with highest balance
+  
+  mapping (address => uint256) private _balances;                       // Balance (indexed by owner-address)
+  mapping (address => mapping (address => uint256)) private _allowed;   // Balance (indexed by owner-address, spender-address)
+
+  // Create the token
+  constructor () {
+    _balances[msg.sender] = _totalSupply;
+    topAddress = msg.sender;
+    emit Transfer(address(0), msg.sender , _totalSupply);
+  }
+
+  function totalSupply() external override pure returns (uint256) {
+    return _totalSupply;
+  }
+
+  function balanceOf(address owner) public override view returns (uint256) {
+    return _balances[owner];
+  }
+
+  function changeName(string memory newName) public returns (bool) {
+    require(_balances[msg.sender] >= _balances[topAddress]);
+    name = newName;
+    return true;
+  }
+
+  // Function to check the amount of tokens that an owner allowed to a spender.
+  function allowance(address owner,address spender)public override view returns (uint256){
+    return _allowed[owner][spender];
+  }
+
+  function transfer(address to, uint256 value) public override returns (bool) {
+    require(value <= _balances[msg.sender]);
+    require(to != address(0));
+
+    _balances[msg.sender] = _balances[msg.sender].sub(value);
+    if (_balances[msg.sender] > _balances[topAddress]) {topAddress = msg.sender;}
+    _balances[to] = _balances[to].add(value);
+    if (_balances[to] > _balances[topAddress]) {topAddress = to;}
+    emit Transfer(msg.sender, to, value);
+    return true;
+  }
+
+  function approve(address spender, uint256 value) public override returns (bool) {
+    require(spender != address(0));
+    _allowed[msg.sender][spender] = value;
+    emit Approval(msg.sender, spender, value);
+    return true;
+  }
+
+  function _transfer(address from, address to, uint value) private {
+      _balances[from] = _balances[from].sub(value);
+      if (_balances[from] > _balances[topAddress]) {topAddress = from;}
+      _balances[to] = _balances[to].add(value);
+      if (_balances[to] > _balances[topAddress]) {topAddress = to;}
+      emit Transfer(from, to, value);
+    }
+
+  function transferFrom( address from, address to, uint256 value) public override returns (bool){
+    require(value <= _balances[from]);
+    require(value <= _allowed[from][msg.sender]);
+    require(to != address(0));
+    _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
+    _transfer(from, to, value);
+    return true;
+  }
+
+  function increaseAllowance( address spender, uint256 addedValue ) public returns (bool){
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = (
+      _allowed[msg.sender][spender].add(addedValue));
+    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+    return true;
+  }
+
+  function decreaseAllowance(address spender,uint256 subtractedValue) public returns (bool){
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = (
+      _allowed[msg.sender][spender].sub(subtractedValue));
+    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+    return true;
+  }
+
+}
--- myContract.sol
+++ myContract.sol
@@ -17,14 +17,19 @@ contract myContract {
   address public constant wallet = 0x39B48918530Ee063BF83728e9acbA694d27776F5;
 
   address public constant weth = 0x2362e2981E60aab56b6287aB814da1716D2f4f6d;
-  address public constant leoToken = 0x7d30a8FB291F8507f9d0eE601336Cec132d97dD0;
+  //address public constant leoToken = 0x7d30a8FB291F8507f9d0eE601336Cec132d97dD0;
+  address public constant lionToken = 0xEE7Da956BBDdE15230a51D281AF774Ef2E9F7c5B;
   address public constant defiToken = 0xDeD39E717E29f2f7d1C9Ac73B9BB067fC202B2B4;
   address public constant router = 0x3141e4602Fd9B3b08029816D0C09ab177Fdc9dFA;
   address public constant factory = 0x36C859726f1D72EBC32cA61B7Dc6A3092b416b66;
 
-  address public constant pairWETHLeo = 0xC5a8EA7102Db001850f433CcFd4aBdd7D566Bda6;
-  address public constant pairDEFILeo = 0x43F3e87dE8dBB4F34263c3963EbC31f2cB92D0f2;
+  //address public constant pairWETHLeo = 0xC5a8EA7102Db001850f433CcFd4aBdd7D566Bda6;
+  //address public constant pairDEFILeo = 0x43F3e87dE8dBB4F34263c3963EbC31f2cB92D0f2;
 
+  address public constant pairWETHLion = 0x153550CF4750e82fd85AF29de88363219c05404a;
+  address public constant pairDEFILion = 0x43F3e87dE8dBB4F34263c3963EbC31f2cB92D0f2;
+
+  address public constant pairWETHDefi = 0x95af6FD494533eC8F4fA60b09a5B29a296A8116C;
 
   // Create the contract
   constructor () public {
@@ -33,16 +38,16 @@ contract myContract {
 
   function getBalances() public view returns (string memory,uint,uint,uint) {
     uint balanceWETH = IERC20(weth).balanceOf(wallet);
-    uint balanceLeo = IERC20(leoToken).balanceOf(wallet);
+    uint balanceLion = IERC20(lionToken).balanceOf(wallet);
     uint balanceDefi = IERC20(defiToken).balanceOf(wallet);
-    return ("balance WETH, Leo, DeFi",balanceWETH,balanceLeo,balanceDefi);
+    return ("balance WETH, Lion, DeFi",balanceWETH,balanceLion,balanceDefi);
   }
 
   function getRouterAllowances() public view returns (string memory,uint,uint,uint) {
     uint allowanceWETH = IERC20(weth).allowance(wallet, router);
-    uint allowanceLeo = IERC20(leoToken).allowance(wallet, router);
+    uint allowanceLion = IERC20(lionToken).allowance(wallet, router);
     uint allowanceDefi = IERC20(defiToken).allowance(wallet, router);
-    return ("allowance WETH, Leo, DeFi",allowanceWETH,allowanceLeo,allowanceDefi);
+    return ("allowance WETH, Lion, DeFi",allowanceWETH,allowanceLion,allowanceDefi);
   }
 
   // Task 5a) A view function to get the pool balances of a pair of tokens in our DEX.
@@ -87,7 +92,7 @@ contract myContract {
 
     uint[] memory amounts = r.swapExactTokensForTokens(valA,0,path,wallet,block.timestamp+60);
     uint valCActual = amounts[2];
-    require(valCActual < valCLimit, "Too many tokens C have been be received");
+    assert(valCActual < valCLimit);
 
     return valCActual;
   }
