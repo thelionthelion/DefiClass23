9075dd6d3b348e30d394affdd82ed4aae51a7191
thelionthelion <widmerleo@student.ethz.ch>
2023-05-09 00:48:12 +0200

    finish lion token setup

--- myContract.sol
+++ myContract.sol
@@ -20,6 +20,7 @@ contract myContract {
   //address public constant leoToken = 0x7d30a8FB291F8507f9d0eE601336Cec132d97dD0;
   address public constant lionToken = 0xEE7Da956BBDdE15230a51D281AF774Ef2E9F7c5B;
   address public constant defiToken = 0xDeD39E717E29f2f7d1C9Ac73B9BB067fC202B2B4;
+  address public constant luluToken = 0x4A79aFCcfC998b95b78772fCa139B3430F8b31e6;
   address public constant router = 0x3141e4602Fd9B3b08029816D0C09ab177Fdc9dFA;
   address public constant factory = 0x36C859726f1D72EBC32cA61B7Dc6A3092b416b66;
 
@@ -27,7 +28,7 @@ contract myContract {
   //address public constant pairDEFILeo = 0x43F3e87dE8dBB4F34263c3963EbC31f2cB92D0f2;
 
   address public constant pairWETHLion = 0x153550CF4750e82fd85AF29de88363219c05404a;
-  address public constant pairDEFILion = 0x43F3e87dE8dBB4F34263c3963EbC31f2cB92D0f2;
+  address public constant pairDEFILion = 0xC8E6457Dd70c32FCFe7d77D9908CAf209A590349;
 
   address public constant pairWETHDefi = 0x95af6FD494533eC8F4fA60b09a5B29a296A8116C;
 
